61693de8ff6b557da5275cde45f4bc8058774f4a
Felipe Matos <felipearmat@gmail.com>
2019-10-22 20:48:27 -0300

    Adicionados Pacotes de projetos e mudança em views
    Alterados pacotes no Vue.js;
    Alterados pacotes no Sails;
    Criado arquivo de configuração do sails autoreload;
    Removido index.html do projeto Sails;
    Alterado layouts.ejs e homepage.ejs para apresentarem o projeto vue;

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,11 @@
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import Main from './Main.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
